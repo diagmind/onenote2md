9c7474a7d9761b27803da33e7ea01a50f10ea3fb
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2025-05-24 18:53:48 +0800

    Update kcms0.php

--- kcms0.php
+++ kcms0.php
@@ -3,11 +3,11 @@
         "text": "\u8f38\u5165",
         "items": [
             {
-                "text": "\u5916\u79d1",
-                "link": "\u5916\u79d1.docx"
-            },{
                 "text": "\u5152\u79d1",
                 "link": "\u5152\u79d1.docx"
+            },{
+                "text": "\u5916\u79d1",
+                "link": "\u5916\u79d1.docx"
             }
         ]
     },
@@ -15,12 +15,11 @@
         "text": "\u8f38\u51fa",
         "items": [
             {
-                "text": "\u5916\u79d1",
-                "link": "\/SurgicalDepartment"
-            },
-            {
                 "text": "\u5152\u79d1",
                 "link": "\/ChildDepartment"
+            }, {
+                "text": "\u5916\u79d1",
+                "link": "\/SurgicalDepartment"
             }
         ]
     }
